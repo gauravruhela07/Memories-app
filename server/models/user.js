aee45f26c61ebf9510de8b0be25ec18ee7f88405
Gaurav Ruhela <gauravruhela@Gauravs-MacBook-Pro.local>
2021-06-13 11:55:54 +0530

    Minor bug fixes

--- server/models/user.js
+++ server/models/user.js
@@ -7,4 +7,5 @@ const userSchema = mongoose.Schema({
     id: {type: String}
 });
 
-export default mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
