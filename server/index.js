fee1306eb01df434509a8f85ba469577f25ad7ce
Gaurav Ruhela <gauravruhela@Gauravs-MacBook-Pro.local>
2021-06-13 10:15:47 +0530

    Added JWT-based signin and signup backend

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 dotenv.config();
@@ -14,14 +15,17 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: "true"}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Hello to Memories API');
 });
 
 const PORT = process.env.PORT || 5000;
+const URL = process.env.CONNECTION_URL;
+const localUrl = 'mongodb://localhost:27017/memories_app'
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(localUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch(err => console.log(err.message));
 
